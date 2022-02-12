6491ca8ded70d3a26b2a2570d87e398e724dc8a4
frankfang <frankfang1990@gmail.com>
2022-02-12 13:46:28 +0800

    remove chrome

--- locate.go
+++ locate.go
@@ -26,23 +26,23 @@ func LocateChrome() string {
 	switch runtime.GOOS {
 	case "darwin":
 		paths = []string{
-			"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-			"/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
-			"/Applications/Chromium.app/Contents/MacOS/Chromium",
+			// "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+			// "/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary",
+			// "/Applications/Chromium.app/Contents/MacOS/Chromium",
 			"/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge",
-			"/usr/bin/google-chrome-stable",
-			"/usr/bin/google-chrome",
-			"/usr/bin/chromium",
-			"/usr/bin/chromium-browser",
+			// "/usr/bin/google-chrome-stable",
+			// "/usr/bin/google-chrome",
+			// "/usr/bin/chromium",
+			// "/usr/bin/chromium-browser",
 		}
 	case "windows":
 		paths = []string{
-			os.Getenv("LocalAppData") + "/Google/Chrome/Application/chrome.exe",
-			os.Getenv("ProgramFiles") + "/Google/Chrome/Application/chrome.exe",
-			os.Getenv("ProgramFiles(x86)") + "/Google/Chrome/Application/chrome.exe",
-			os.Getenv("LocalAppData") + "/Chromium/Application/chrome.exe",
-			os.Getenv("ProgramFiles") + "/Chromium/Application/chrome.exe",
-			os.Getenv("ProgramFiles(x86)") + "/Chromium/Application/chrome.exe",
+			// os.Getenv("LocalAppData") + "/Google/Chrome/Application/chrome.exe",
+			// os.Getenv("ProgramFiles") + "/Google/Chrome/Application/chrome.exe",
+			// os.Getenv("ProgramFiles(x86)") + "/Google/Chrome/Application/chrome.exe",
+			// os.Getenv("LocalAppData") + "/Chromium/Application/chrome.exe",
+			// os.Getenv("ProgramFiles") + "/Chromium/Application/chrome.exe",
+			// os.Getenv("ProgramFiles(x86)") + "/Chromium/Application/chrome.exe",
 			os.Getenv("ProgramFiles(x86)") + "/Microsoft/Edge/Application/msedge.exe",
 			os.Getenv("ProgramFiles") + "/Microsoft/Edge/Application/msedge.exe",
 		}
